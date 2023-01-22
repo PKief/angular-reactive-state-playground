cdec90e38894b7e09aae0791f8e2c77fea9b4565
Philipp Kief <philipp.kief@gmx.de>
2023-01-22 16:01:32 +0100

    Add store service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Todo, TodoStoreService } from './services/todo-store.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'todo-app';
+  readonly completedTodos$: Observable<Todo[]>;
+  readonly uncompletedTodos$: Observable<Todo[]>;
+
+  constructor(private todoStore: TodoStoreService) {
+    const allTodos$ = this.todoStore.select(state => state.todos);
+    this.completedTodos$ = allTodos$.pipe(
+      map(todos => todos.filter(t => !!t.completed))
+    );
+    this.uncompletedTodos$ = allTodos$.pipe(
+      map(todos => todos.filter(t => !t.completed))
+    );
+  }
+
+  addTodo(description: string) {
+    this.todoStore.addTodo(description);
+  }
+
+  completeTodo(todo: Todo) {
+    this.todoStore.completeTodo(todo);
+  }
+
+  uncompleteTodo(todo: Todo) {
+    this.todoStore.uncompleteTodo(todo);
+  }
 }
